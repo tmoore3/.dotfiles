1f43dae36a991c704aeb4e06d13cb05d49b32f31
Tom Moore <tomwmoore@hotmail.com>
2024-04-02 12:40:04 +0100

    Enabled system clipboard copying to nvim.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -525,3 +525,8 @@ cmp.setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- Custom
+
+-- Enable system clipboard copying
+set clipboard=unnamedplus
